9505fa1925736979605f3c814a22a7f0058e6e85
CORPHQ-HK-PCCW\01734821 <alex.kt.so@pccw.com>
2022-03-04 15:12:07 +0800

    fix .then and sync issues

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-
 const { execute, log, program } = require('./run/execute');
 
-(async () => {    
+try {
     program
-    .version('0.0.1')
-    .argument('<mode>', 'mode to run')
-    .description('Generate Java classes from Excel')
-    .action((mode) => {        
-        execute(mode);
-    });
+        .version('0.0.1')
+        .argument('<mode>', 'mode to run')
+        .description('Generate Java classes from Excel')
+        .action((mode) => {
+            execute(mode);
+        });
 
-    await program.parseAsync(process.argv);    
-})().then(() => {
+    program.parseAsync(process.argv);
     log.info('Finished');
-}).catch(e => {    
+
+} catch (e) {
     log.error(e);
-});
-
-
+}
