184e1ee8238923a9fe2787973e4833d6f3c24e71
CORPHQ-HK-PCCW\01734821 <alex.kt.so@pccw.com>
2022-03-04 11:23:22 +0800

    support entity/function begin name csv

--- run/execute.js
+++ run/execute.js
@@ -1,36 +1,38 @@
 const distinct = require('distinct');
 const setting = require('../config/setting');
-const { fieldNameToJavaName, getCSVObj, setMode } = require('../lib/commonFunct');
+const { fieldNameToJavaName, getCSVObj, setMode, getCSVFilesFromPath } = require('../lib/commonFunct');
 const fileExists = require('file-exists');
 const { handleEntityFile, handleFunctionFile } = require('../lib/outputFile');
 const Logger = require('simple-node-logger'),
-	opts = {
-		logFilePath:'genJavaFromExcel.log',
-		timestampFormat:'YYYY-MM-DD HH:mm:ss'
-	},
-log = Logger.createSimpleLogger( opts );
+    opts = {
+        logFilePath: 'genJavaFromExcel.log',
+        timestampFormat: 'YYYY-MM-DD HH:mm:ss'
+    },
+    log = Logger.createSimpleLogger(opts);
 const { Command } = require('commander');
 const program = new Command();
 
 const execute = async (curMode) => {
-    const entityFile = `${setting.inputPath}entity.csv`;
-    const functionFile = `${setting.inputPath}function.csv`;
+    const csvFiles = getCSVFilesFromPath(setting.inputPath);
+
+    const entityFile = `${setting.inputPath}${csvFiles.find(x => x.replace('entity') !== x)}`;
+    const functionFile = `${setting.inputPath}${csvFiles.find(x => x.replace('function') !== x)}`;
     let distinctWorkStreamList = [];
-    
+
     curMode && setMode(curMode);
 
     const entityFileExists = fileExists.sync(entityFile);
     const functionFileExists = fileExists.sync(functionFile);
 
     if (entityFileExists) {
-        const entityObj = await getCSVObj(entityFile);    
+        const entityObj = await getCSVObj(entityFile);
 
         const tableList = entityObj.map(x => fieldNameToJavaName(x["Table Name"], true));
         const workStreamList = entityObj.map(x => x["Workstream"]);
-    
+
         const distinctTableList = distinct(tableList);
         distinctWorkStreamList = distinct(workStreamList);
-    
+
         handleEntityFile(distinctWorkStreamList, distinctTableList, entityObj);
     }
 
